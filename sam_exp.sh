63cf66d5a6960b0fa60ccc7afe81c772a86f4d86
samjkwong <samkwong@stanford.edu>
2020-11-14 21:00:02 +0000

    Fix typo

--- sam_exp.sh
+++ sam_exp.sh
@@ -11,26 +11,26 @@
 #python maml_demo.py --loss low  --labeller random --weak_prob 0.99 --weak_coefficient 1.0  --num_iterations 500 --test_set_weakness True --dataset omniglot
 
 echo 'Standard correct_prob=0.25'
-python maml_demo.py --loss low --labeller random --num_iterations 2000 --correct_prob 0.25 --weak_coefficient 1 --weak_prob 0.8  --test_set_weakness True --dataset omiglot
+python maml_demo.py --loss low --labeller random --num_iterations 1000 --correct_prob 0.25 --weak_coefficient 1 --weak_prob 0.8  --test_set_weakness True --dataset omniglot
 echo ''
 
 echo 'Standard correct_prob=0.5'
-python maml_demo.py --loss low --labeller random --num_iterations 2000 --correct_prob 0.5 --weak_coefficient 1 --weak_prob 0.8  --test_set_weakness True --dataset omiglot
+python maml_demo.py --loss low --labeller random --num_iterations 1000 --correct_prob 0.5 --weak_coefficient 1 --weak_prob 0.8  --test_set_weakness True --dataset omniglot
 echo ''
 
 echo 'Standard correct_prob=0.8'
-python maml_demo.py --loss low --labeller random --num_iterations 2000 --correct_prob 0.8 --weak_coefficient 1 --weak_prob 0.8  --test_set_weakness True --dataset omiglot
+python maml_demo.py --loss low --labeller random --num_iterations 1000 --correct_prob 0.8 --weak_coefficient 1 --weak_prob 0.8  --test_set_weakness True --dataset omniglot
 echo ''
 
 echo 'Weak correct_prob=0.25'
-python maml_demo.py --loss low --labeller random --num_iterations 2000 --correct_prob 0.25 --weak_coefficient 0.4 --weak_prob 0.8  --test_set_weakness True --dataset omiglot
+python maml_demo.py --loss low --labeller random --num_iterations 1000 --correct_prob 0.25 --weak_coefficient 0.4 --weak_prob 0.8  --test_set_weakness True --dataset omniglot
 echo ''
 
 echo 'Weak correct_prob=0.5'
-python maml_demo.py --loss low --labeller random --num_iterations 2000 --correct_prob 0.5 --weak_coefficient 0.4 --weak_prob 0.8  --test_set_weakness True --dataset omiglot
+python maml_demo.py --loss low --labeller random --num_iterations 1000 --correct_prob 0.5 --weak_coefficient 0.4 --weak_prob 0.8  --test_set_weakness True --dataset omniglot
 echo ''
 
 echo 'Weak correct_prob=0.8'
-python maml_demo.py --loss low --labeller random --num_iterations 2000 --correct_prob 0.8 --weak_coefficient 0.4 --weak_prob 0.8  --test_set_weakness True --dataset omiglot
+python maml_demo.py --loss low --labeller random --num_iterations 1000 --correct_prob 0.8 --weak_coefficient 0.4 --weak_prob 0.8  --test_set_weakness True --dataset omniglot
 echo ''
 
