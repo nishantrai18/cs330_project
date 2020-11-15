b27362957da8c2503c487d858c38752b24a7c7de
samjkwong <samkwong@stanford.edu>
2020-11-15 04:51:16 +0000

    Fix typo

--- sam_exp.sh
+++ sam_exp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo 'proto correct_prob=0.0'
-python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.0 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --featured_to_use fets
+python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.0 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --features_to_use fets
 echo ''
 
 echo 'proto correct_prob=0.25'
-python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.25 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --featured_to_use fets
+python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.25 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --features_to_use fets
 echo ''
 
 echo 'proto correct_prob=0.5'
-python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.5 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --featured_to_use fets
+python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.5 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --features_to_use fets
 echo ''
 
 echo 'proto correct_prob=0.8'
-python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.8 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --featured_to_use fets
+python maml_demo.py --loss proto --labeller random --num_iterations 1000 --correct_prob 0.8 --weak_coefficient 0.4 --weak_prob 0.8  --dataset omniglot --test_set_weakness True  --features_to_use fets
 echo ''
 
